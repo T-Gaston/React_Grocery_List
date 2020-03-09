d7e2862f1aa81aa5c4b6e157e2854dbc78c3c0dd
Thomas Gaston <thomasgaston11@gmail.com>
2020-03-09 15:59:36 -0600

    grocery list displays properly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './GroceryList';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+    state = {
+      groceries: [
+        { id: 1, name: "bananas", complete: true, },
+        { id: 2, name: "apples", complete: false, },
+        { id: 3, name: "oranges", complete: true, },
+      ]
+    };
+  
+  render() {
+    const { groceries } = this.state;
+
+    return(
+        <List name="Grocery List" items={groceries} />
+    );
+  }
 }
 
 export default App;
