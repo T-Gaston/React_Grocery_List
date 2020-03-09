f1f216066554c6d2675e043904f62b38a413e7de
Thomas Gaston <thomasgaston11@gmail.com>
2020-03-09 17:00:58 -0600

    working grocery list app, no extra features

--- src/App.js
+++ src/App.js
@@ -2,21 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import List from './GroceryList';
+import GroceryForm from './GroceryForm';
 
 class App extends React.Component {
-    state = {
-      groceries: [
-        { id: 1, name: "bananas", complete: true, },
-        { id: 2, name: "apples", complete: false, },
-        { id: 3, name: "oranges", complete: true, },
-      ]
-    };
+    state = { groceries: [] }
+
+    getUniqId = () => {
+      return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+    }
+  
+    addItem = (name) => {
+      const { groceries } = this.state;
+      const grocery = { name, id: this.getUniqId() , complete: false }
+      this.setState({ groceries: [grocery, ...groceries] });
+    }
+
+
+    handleClick = (id) => {
+      const { groceries } = this.state;
+      this.setState({
+        groceries: groceries.map( grocery => {
+          if (grocery.id === id) {
+            return {
+              ...grocery,
+              complete: !grocery.complete
+            }
+          }
+          return grocery
+        })
+      })
+    }
   
   render() {
     const { groceries } = this.state;
 
     return(
-        <List name="Grocery List" items={groceries} />
+      <div>
+        <GroceryForm addItem={this.addItem} />
+        <List name="Grocery List" items={groceries} groceryClick={this.handleClick} />
+      </div>
     );
   }
 }
--- src/GroceryList.js
+++ src/GroceryList.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import Grocery from './Grocery'
 
 
-const List = ({ items, name}) => (
+const List = ({ items, name, groceryClick }) => (
   <div>
     <h2>{name}</h2>
     <ul>
-    { items.map( item => <li key={item.id}>{item.name}</li>) }
+    { items.map( item => <Grocery key={item.id} {...item} groceryClick={groceryClick} /> ) }
     </ul>
   </div>
-);
+)
 
 
 export default List;
